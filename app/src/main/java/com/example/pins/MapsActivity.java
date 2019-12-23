1cec4d8297bbfa2a0adef78473217afe553219f0
SimeonHG <simeonn.h.georgiev@gmail.com>
2019-12-23 14:36:43 +0200

    diferent pin for the user and now showing only friends + a circle around the user to indicate radius

--- app/src/main/java/com/example/pins/MapsActivity.java
+++ app/src/main/java/com/example/pins/MapsActivity.java
@@ -6,6 +6,7 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
@@ -19,6 +20,9 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -34,17 +38,20 @@ import com.google.firebase.database.ValueEventListener;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
     private LocationManager locationManager;
-    private DatabaseReference locationsRef;
+    private DatabaseReference locationsRef, dbRef;
     private FirebaseAuth mAuth;
     private String currentUserID;
+    private  LatLng currentLatLng;
+    private Marker currentLoc;
+    private boolean flag;
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,12 +61,35 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mAuth = FirebaseAuth.getInstance();
         currentUserID = mAuth.getCurrentUser().getUid();
         locationsRef = FirebaseDatabase.getInstance().getReference("Locations");
+        dbRef = FirebaseDatabase.getInstance().getReference();
 
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+
+
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+       // LatLng sydney = new LatLng(-34, 151);
+       // LatLng currentLoc = new LatLng(LOCATION_SERVICE.)
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
@@ -76,26 +106,51 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 0, new LocationListener() {
                 @Override
                 public void onLocationChanged(Location location) {
-                    //TODO update-ni lastKnownLoc vuv firebase?
 
-                    double latitude = location.getLatitude();
-                    double longitude = location.getLongitude();
+                    final double latitude = location.getLatitude();
+                    final double longitude = location.getLongitude();
 
-                    LatLng currentLatLng = new LatLng(latitude, longitude);
+                    currentLatLng = new LatLng(latitude, longitude);
 
                     locationsRef.child(currentUserID).child("last_location").child("latitude").setValue(latitude);
                     locationsRef.child(currentUserID).child("last_location").child("longitude").setValue(longitude);
 
-                   // final List<LatLng> allFriendsLocations = new ArrayList<>();
-                    locationsRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    // final List<LatLng> allFriendsLocations = new ArrayList<>();
+                    locationsRef.addValueEventListener(new ValueEventListener() {
                         @Override
                         public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                             mMap.clear();
+                            currentLoc = mMap.addMarker(new MarkerOptions()
+                                    .position(new LatLng(latitude,longitude))
+                                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+                            );                            mMap.addCircle(new CircleOptions()
+                                    .center(currentLatLng)
+                                    .radius(500.0)
+                                    .fillColor(Color.argb(70, 150, 50, 50)));
                             for(DataSnapshot snapshot : dataSnapshot.getChildren()){
-                                double db_latitude = (double) snapshot.child("latitude").getValue();
-                                double db_longitude = (double) snapshot.child("longitude").getValue();
-                                LatLng latLng = new LatLng(db_latitude, db_longitude);
-                                mMap.addMarker(new MarkerOptions().position(latLng));
+                                 Toast.makeText(MapsActivity.this, "hellooooooo", Toast.LENGTH_LONG).show();
+                                double db_latitude = (double) snapshot.child("last_location").child("latitude").getValue();
+                                double db_longitude = (double) snapshot.child("last_location").child("longitude").getValue();
+                                final LatLng latLng = new LatLng(db_latitude, db_longitude);
+                                if(latitude != db_latitude && longitude!=db_longitude) {
+                                    final String userID = snapshot.getKey();
+                                    dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                                        @Override
+                                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                            if (dataSnapshot.child("Friendlists").child(currentUserID).hasChild(userID)) {
+                                                //Toast.makeText(MapsActivity.this, currentUserID, Toast.LENGTH_LONG).show();
+                                                //System.out.println(flag);
+                                                mMap.addMarker(new MarkerOptions().position(latLng));
+
+                                            }
+                                        }
+
+                                        @Override
+                                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                        }
+                                    });
+                                }
                                 //allFriendsLocations.add(latLng);
                             }
                         }
@@ -108,6 +163,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
                     mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLatLng));
+
                 }
 
                 @Override
@@ -131,25 +187,55 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 public void onLocationChanged(Location location) {
                     //TODO update-ni lastKnownLoc vuv firebase?
 
-                    double latitude = location.getLatitude();
-                    double longitude = location.getLongitude();
+                    final double latitude = location.getLatitude();
+                    final double longitude = location.getLongitude();
 
-                    LatLng currentLatLng = new LatLng(latitude, longitude);
+                    currentLatLng = new LatLng(latitude, longitude);
 
                     locationsRef.child(currentUserID).child("last_location").child("latitude").setValue(latitude);
                     locationsRef.child(currentUserID).child("last_location").child("longitude").setValue(longitude);
+                    currentLoc = mMap.addMarker(new MarkerOptions().position(new LatLng(latitude,longitude)));
 
                     //final List<LatLng> allFriendsLocations = new ArrayList<>();
-                    locationsRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    locationsRef.addValueEventListener(new ValueEventListener() {
                         @Override
                         public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                             mMap.clear();
+                            currentLoc = mMap.addMarker(new MarkerOptions()
+                                    .position(new LatLng(latitude,longitude))
+                                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+                            );
+
+                            mMap.addCircle(new CircleOptions()
+                                    .center(currentLatLng)
+                                    .radius(500.0)
+                                    .fillColor(Color.argb(70, 150, 50, 50))
+                            );
                             for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                                //  Toast.makeText(MapsActivity.this, "hellooooooo", Toast.LENGTH_LONG).show();
                                 double db_latitude = (double) snapshot.child("last_location").child("latitude").getValue();
                                 double db_longitude = (double) snapshot.child("last_location").child("longitude").getValue();
-                                LatLng latLng = new LatLng(db_latitude, db_longitude);
+                                final LatLng latLng = new LatLng(db_latitude, db_longitude);
                                 //allFriendsLocations.add(latLng);
-                                mMap.addMarker(new MarkerOptions().position(latLng));
+                                if(latitude != db_latitude && longitude!=db_longitude) {
+                                    final String userID = snapshot.getKey();
+                                    dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                                        @Override
+                                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                            if (dataSnapshot.child("Friendlists").child(currentUserID).hasChild(userID)) {
+                                                //Toast.makeText(MapsActivity.this, currentUserID, Toast.LENGTH_LONG).show();
+                                                //System.out.println(flag);
+                                                mMap.addMarker(new MarkerOptions().position(latLng));
+
+                                            }
+                                        }
+
+                                        @Override
+                                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                        }
+                                    });
+                                }
                             }
                         }
 
@@ -159,7 +245,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         }
                     });
 
-                    mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLatLng));
+                    mMap.animateCamera(CameraUpdateFactory.newLatLng(currentLatLng));
+
+
                 }
 
                 @Override
@@ -179,26 +267,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             });
         }
 
+
     }
 
+    private boolean IsFriend(final String userID) {
+        flag = false;
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        ValueEventListener listener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.child("Friendlists").child(currentUserID).hasChild(userID)) {
+                    //Toast.makeText(MapsActivity.this, currentUserID, Toast.LENGTH_LONG).show();
+                    //System.out.println(flag);
+                    flag = true;
 
-        // Add a marker in Sydney and move the camera
-       // LatLng sydney = new LatLng(-34, 151);
-       // LatLng currentLoc = new LatLng(LOCATION_SERVICE.)
+                }
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
+            }
+        };
+
+        dbRef.addListenerForSingleValueEvent(listener);
+
+        return flag;
     }
 }
